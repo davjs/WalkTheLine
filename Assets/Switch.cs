5ba799d8ae78bc2b70c4e26f1a9135c466b874e5
augustalex <augustalexandersson@gmail.com>
2018-05-26 18:04:04 +0200

    Opened project with Unity 2018.1

---
+++ Assets/Switch.cs
@@ -0,0 +1,28 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+	public GameObject RoadA;
+	public GameObject RoadB;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void SwitchRoadForCube(GameObject cube)
+	{
+		
+	}
+
+}
